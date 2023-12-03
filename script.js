b396107411107a30f93d97a8510fc5c7d581778f
andermendz <andersonmendz19@gmail.com>
2023-12-03 17:54:04 -0500

    todo: simplify the code and project

--- script.js
+++ script.js
@@ -38,12 +38,58 @@ formbutton.onclick = function (e) {
   const book = document.createElement("div");
   book.className = "book";
 
+  const bookimage = document.createElement("div");
+  bookimage.className = "book-image";
+  const image = document.createElement("img");
+  image.alt = "image here";
+  image.src = "./media/b1.jpg"
+  bookimage.appendChild(image);
+
+  book.appendChild(bookimage);
+
+  const bookdetails = document.createElement("div");
+  bookdetails.className = "book-details";
+
+  const booktitle = document.createElement("div");
+  booktitle.className = "book-title";
+  booktitle.textContent = bookObject.title;
+  bookdetails.appendChild(booktitle);
+ 
+  const bookauthor = document.createElement("div");
+  bookauthor.className = "book-author";
+  bookauthor.textContent = "By " + bookObject.author;
+  bookdetails.appendChild(bookauthor);
+
+  const booknpages = document.createElement("div");
+  booknpages.className = "book-npages";
+
+  const pagesindicator = document.createElement("div");
+  pagesindicator.textContent = "Pages:";
+  booknpages.appendChild(pagesindicator);
+
+  const pagesnumber = document.createElement("div");
+  pagesnumber.textContent = bookObject.npages;
+  booknpages.appendChild(pagesnumber);
+  bookdetails.appendChild(booknpages);
+  book.appendChild(bookdetails)
+
+  const bookstatus = document.createElement("div");
+  bookstatus.className = "book-status";
+
+  const statusTitle = document.createElement("div");
+  statusTitle.textContent = "Status:";
+  bookstatus.appendChild(statusTitle);
+
+  const statusIndicator = document.createElement("div");
+  statusIndicator.className = "status-indicator";
+  statusIndicator.textContent = bookObject.status;
+  bookstatus.appendChild(statusIndicator);
+  bookdetails.appendChild(bookstatus);
+
+
+  book.appendChild(bookdetails)
+
+
   bookscontainer.appendChild(book)
 
-  console.log(bookscontainer)
-
-  
-  
-  console.log(bookStatus);
-  console.log(book);
 };
