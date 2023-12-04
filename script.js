ca222a80cf7fa16aa1979fe365a1941b713edbf8
andermendz <andersonmendz19@gmail.com>
2023-12-03 19:21:16 -0500

    simplified code and UI

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ Book = function (title, author, npages, status) {
   this.status = status;
 };
 
+const booksform = document.getElementById('booksform');
 const formbutton = document.getElementById("form_button");
 
-
-
 formbutton.onclick = function (e) {
-    e.preventDefault();
+  e.preventDefault();
   const formTitle = document.getElementById("title");
   const formAuthor = document.getElementById("author");
   const formPages = document.getElementById("npages");
@@ -28,7 +27,7 @@ formbutton.onclick = function (e) {
     }
   });
 
-   bookObject = new Book(
+  bookObject = new Book(
     formTitle.value,
     formAuthor.value,
     formPages.value,
@@ -38,15 +37,6 @@ formbutton.onclick = function (e) {
   const book = document.createElement("div");
   book.className = "book";
 
-  const bookimage = document.createElement("div");
-  bookimage.className = "book-image";
-  const image = document.createElement("img");
-  image.alt = "image here";
-  image.src = "./media/b1.jpg"
-  bookimage.appendChild(image);
-
-  book.appendChild(bookimage);
-
   const bookdetails = document.createElement("div");
   bookdetails.className = "book-details";
 
@@ -54,7 +44,7 @@ formbutton.onclick = function (e) {
   booktitle.className = "book-title";
   booktitle.textContent = bookObject.title;
   bookdetails.appendChild(booktitle);
- 
+
   const bookauthor = document.createElement("div");
   bookauthor.className = "book-author";
   bookauthor.textContent = "By " + bookObject.author;
@@ -71,7 +61,7 @@ formbutton.onclick = function (e) {
   pagesnumber.textContent = bookObject.npages;
   booknpages.appendChild(pagesnumber);
   bookdetails.appendChild(booknpages);
-  book.appendChild(bookdetails)
+  book.appendChild(bookdetails);
 
   const bookstatus = document.createElement("div");
   bookstatus.className = "book-status";
@@ -83,13 +73,16 @@ formbutton.onclick = function (e) {
   const statusIndicator = document.createElement("div");
   statusIndicator.className = "status-indicator";
   statusIndicator.textContent = bookObject.status;
+  if (bookObject.status === "Read") {
+    statusIndicator.classList.add("read");
+  } else if (bookObject.status === "Not Read") {
+    statusIndicator.classList.add("not-read");
+  }
   bookstatus.appendChild(statusIndicator);
   bookdetails.appendChild(bookstatus);
 
+  book.appendChild(bookdetails);
 
-  book.appendChild(bookdetails)
-
-
-  bookscontainer.appendChild(book)
-
+  booksform.reset();
+  bookscontainer.appendChild(book);
 };
