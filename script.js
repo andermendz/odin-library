18261a9e06e3ad2f51c494dfdd4e7d61abdabea2
andermendz <andersonmendz19@gmail.com>
2023-12-04 22:13:34 -0500

    array implemented, added comments and examples for tweaks tomorrow (toggle function)

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ Book = function (title, author, npages, status) {
 const booksform = document.getElementById('booksform');
 const formbutton = document.getElementById("form_button");
 
+book.prototype.changeStatus = function () {
+    if(this.status == "Read"){
+      this.status = "Not Read";
+    } else if (this.status == "Not Read"){
+      this.status = "Read";
+    }
+};
+
 formbutton.onclick = function (e) {
   e.preventDefault();
 
@@ -60,68 +68,98 @@ formbutton.onclick = function (e) {
   );
 
   
-let users = [{name: "pedro", age: 212}, {name: "dave", age: 21}, {name:"last person"}]
-users.push(bookObject);
-console.log(users[3].title);
+let books = []
+books.push(bookObject);
+console.log(books);
 
-// modifica para que por cada elemento del array debe haber un  book en la vista
-  const book = document.createElement("div");
-  book.className = "book";
+// se podria añadir funciones especificar que perteneceria a cada libro como la de toggle de estado
+    books.forEach((book) => {
+      const bookdiv = document.createElement("div");
+      bookdiv.className = "book";
+    
+      const bookdetails = document.createElement("div");
+      bookdetails.className = "book-details";
+    
+      const booktitle = document.createElement("div");
+      booktitle.className = "book-title";
+    
+      if(book.title.length > 34){
+        book.title = book.title.slice(0, 34) + "...";
+      }
+      booktitle.textContent = book.title;
+      bookdetails.appendChild(booktitle);
+    
+      const bookauthor = document.createElement("div");
+      bookauthor.className = "book-author";
+      if(book.author.length > 28){
+        book.author = book.author.slice(0, 28) + "...";
+      }
+      bookauthor.textContent = "By " + book.author;
+      bookdetails.appendChild(bookauthor);
+    
+      const booknpages = document.createElement("div");
+      booknpages.className = "book-npages";
+    
+      const pagesindicator = document.createElement("div");
+      pagesindicator.textContent = "Pages:";
+      booknpages.appendChild(pagesindicator);
+    
+      const pagesnumber = document.createElement("div");
+      pagesnumber.textContent = book.npages;
+      booknpages.appendChild(pagesnumber);
+      bookdetails.appendChild(booknpages);
+      bookdiv.appendChild(bookdetails);
+    
+      const bookstatus = document.createElement("div");
+      bookstatus.className = "book-status";
+    
+      const statusTitle = document.createElement("div");
+      statusTitle.textContent = "Status:";
+      bookstatus.appendChild(statusTitle);
+    
+      const statusIndicator = document.createElement("div");
+      statusIndicator.className = "status-indicator";
+      statusIndicator.textContent = book.status;
+      if (book.status === "Read") {
+        statusIndicator.classList.add("read");
+      } else if (book.status === "Not Read") {
+        statusIndicator.classList.add("not-read");
+      }
+      bookstatus.appendChild(statusIndicator);
+      bookdetails.appendChild(bookstatus);
 
-  const bookdetails = document.createElement("div");
-  bookdetails.className = "book-details";
+      const bookActions = document.createElement("div");
+      bookActions.className = "book-actions";
 
-  const booktitle = document.createElement("div");
-  booktitle.className = "book-title";
+      const bookEdit = document.createElement("div");
+      bookEdit.className = "book-edit";
+      
+      const editIcon = document.createElement("span");
+      editIcon.className = "material-symbols-outlined";
+      editIcon.textContent = "edit";
+      bookEdit.appendChild(editIcon);
 
-  if(bookObject.title.length > 34){
-    bookObject.title = bookObject.title.slice(0, 34) + "...";
-  }
-  booktitle.textContent = bookObject.title;
-  bookdetails.appendChild(booktitle);
+      const bookDelete = document.createElement("div");
+      bookDelete.className = "book-delete";
 
-  const bookauthor = document.createElement("div");
-  bookauthor.className = "book-author";
-  if(bookObject.author.length > 28){
-    bookObject.author = bookObject.author.slice(0, 28) + "...";
-  }
-  bookauthor.textContent = "By " + bookObject.author;
-  bookdetails.appendChild(bookauthor);
+      const deleteIcon = document.createElement("span");
+      deleteIcon.className = "material-symbols-outlined";
+      deleteIcon.textContent = "delete";
+      bookDelete.appendChild(deleteIcon);
 
-  const booknpages = document.createElement("div");
-  booknpages.className = "book-npages";
 
-  const pagesindicator = document.createElement("div");
-  pagesindicator.textContent = "Pages:";
-  booknpages.appendChild(pagesindicator);
 
-  const pagesnumber = document.createElement("div");
-  pagesnumber.textContent = bookObject.npages;
-  booknpages.appendChild(pagesnumber);
-  bookdetails.appendChild(booknpages);
-  book.appendChild(bookdetails);
+      bookActions.appendChild(bookEdit);
+      bookActions.appendChild(bookDelete);
 
-  const bookstatus = document.createElement("div");
-  bookstatus.className = "book-status";
+      bookdetails.appendChild(bookActions);
 
-  const statusTitle = document.createElement("div");
-  statusTitle.textContent = "Status:";
-  bookstatus.appendChild(statusTitle);
+      bookdiv.appendChild(bookdetails);
+      bookscontainer.appendChild(bookdiv);
+  
+    });
 
-  const statusIndicator = document.createElement("div");
-  statusIndicator.className = "status-indicator";
-  statusIndicator.textContent = bookObject.status;
-  if (bookObject.status === "Read") {
-    statusIndicator.classList.add("read");
-  } else if (bookObject.status === "Not Read") {
-    statusIndicator.classList.add("not-read");
-  }
-  bookstatus.appendChild(statusIndicator);
-  bookdetails.appendChild(bookstatus);
-
-  book.appendChild(bookdetails);
-
-  booksform.reset();
-  bookscontainer.appendChild(book);
+ 
+    booksform.reset();
 };
 
