46eac2bac7f2b6d5cc28ff06a38cee20492ad33f
andermendz <andersonmendz19@gmail.com>
2023-12-31 23:40:06 -0500

    functional edit prototype

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Book = function (id,title, author, npages, status) {
+Book = function (id, title, author, npages, status) {
   this.id = id;
   this.title = title;
   this.author = author;
@@ -7,10 +7,10 @@ Book = function (id,title, author, npages, status) {
 };
 
 const booksform = document.getElementById("booksform");
+const bookscontainer = document.querySelector(".bookscontainer");
 const formbutton = document.getElementById("form_button");
 let errorContainer = document.createElement("div");
 let dialog = document.querySelector("dialog");
-
 
 Book.prototype.changeStatus = function () {
   if (this.status == "Read") {
@@ -19,52 +19,130 @@ Book.prototype.changeStatus = function () {
     this.status = "Read";
   }
 
-  console.log(this.length)
+  console.log(this.length);
 };
 
 Book.prototype.Remove = function () {
-      books.splice(this.id, 1);
-
-}
+  books.splice(this.id, 1);
+};
 
 Book.prototype.Edit = function () {
-  console.log("The book to edit is the one with ID : " + this.id)
+  console.log("The book to edit is the one with ID : " + this.id);
 
-  let titleDialog = document.getElementById('title-dialog');
-  let authorDialog = document.getElementById('author-dialog');
-  let npagesDialog = document.getElementById('npages-dialog');
+  let titleDialog = document.getElementById("title-dialog");
+  let authorDialog = document.getElementById("author-dialog");
+  let npagesDialog = document.getElementById("npages-dialog");
 
-  let statusReadDialog = document.getElementById('dialog-read');
-  let statusNotReadDialog = document.getElementById('dialog-notread');
+  let statusReadDialog = document.getElementById("dialog-read");
+  let statusNotReadDialog = document.getElementById("dialog-notread");
 
-  this.status == 'Read' ? statusReadDialog.checked = true : statusNotReadDialog.checked = true;
-  
+  this.status == "Read"
+    ? (statusReadDialog.checked = true)
+    : (statusNotReadDialog.checked = true);
+
+  let dialogFormButton = document.getElementById("dialog-form_button");
+
+  dialogFormButton.onclick = (e) => {
+    e.preventDefault();
+
+    this.title = titleDialog.value;
+    dialog.close();
+    
+    renderBooks()
+  };
 
   titleDialog.value = this.title;
   authorDialog.value = this.author;
   npagesDialog.value = this.npages;
+
+
+ 
+};
+
+
+
+function renderBook(book) {
+  const bookdiv = document.createElement("div");
+  bookdiv.innerHTML = `
+    <div class="book">
+      <div class="book-details">
+        <div class="book-title">${
+          book.title.length > 34
+            ? book.title.slice(0, 34) + "..."
+            : book.title
+        }</div>
+        <div class="book-author">By ${
+          book.author.length > 28
+            ? book.author.slice(0, 28) + "..."
+            : book.author
+        }</div>
+        <div class="book-npages">
+          <div>Pages:</div>
+          <div>${book.npages}</div>
+        </div>
+        <div class="book-status">
+          <div>Status:</div>
+          <div class="status-indicator ${
+            book.status === "Read" ? "read" : "not-read"
+          }">${book.status}</div>
+        </div>
+        <div class="book-actions">
+          <div class="book-edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div class="book-delete">
+            <span class="material-symbols-outlined">delete</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const statusIndicator = bookdiv.querySelector(".status-indicator");
+  const bookDelete = bookdiv.querySelector(".book-delete");
+  const bookEdit = bookdiv.querySelector(".book-edit");
+
+  statusIndicator.onclick = function () {
+    book.changeStatus();
+    renderBooks()
+  };
+
+  bookDelete.onclick = function () {
+    book.Remove();
+   
+  };
+
+  bookEdit.onclick = function () {
+    book.Edit();
+    dialog.showModal();
+  };
+
+  bookscontainer.appendChild(bookdiv);
 }
 
+function renderBooks() {
+  bookscontainer.innerHTML = "";
+  books.forEach((book) => {
+    renderBook(book);
+  });
+}
 
 let books = [];
 
 formbutton.onclick = function (e) {
   e.preventDefault();
 
-
-  const bookId = document.getElementById('book-id');
+  const bookId = document.getElementById("book-id");
 
   // declare bookid by the length of the array
   bookId.value = books.length;
-
-  
 
   const formTitle = document.getElementById("title");
   const formAuthor = document.getElementById("author");
   const formPages = document.getElementById("npages");
   let bookStatus = document.getElementsByName("status");
 
-  const bookscontainer = document.querySelector(".bookscontainer");
+  
 
   bookStatus.forEach((status) => {
     {
@@ -91,11 +169,10 @@ formbutton.onclick = function (e) {
     formAuthor.value == "" ||
     formPages.value == ""
   ) {
-    
     errorContainer.setAttribute("class", "error-container");
     booksform.appendChild(errorContainer);
     errorContainer.textContent = "Please fill in all the fields";
-   
+
     console.log("error en el formulario");
     return;
   } else if (
@@ -109,7 +186,7 @@ formbutton.onclick = function (e) {
     booksform.appendChild(errorContainer);
 
     errorContainer.textContent = "Choose a status";
-   
+
     console.log("no status");
     return;
   } else {
@@ -127,82 +204,11 @@ formbutton.onclick = function (e) {
   books.push(bookObject);
   console.log(books);
 
-  
-  function renderBook(book) {
-    const bookdiv = document.createElement("div");
-    bookdiv.innerHTML = `
-      <div class="book">
-        <div class="book-details">
-          <div class="book-title">${book.title.length > 34 ? book.title.slice(0, 34) + "..." : book.title}</div>
-          <div class="book-author">By ${book.author.length > 28 ? book.author.slice(0, 28) + "..." : book.author}</div>
-          <div class="book-npages">
-            <div>Pages:</div>
-            <div>${book.npages}</div>
-          </div>
-          <div class="book-status">
-            <div>Status:</div>
-            <div class="status-indicator ${book.status === "Read" ? "read" : "not-read"}">${book.status}</div>
-          </div>
-          <div class="book-actions">
-            <div class="book-edit">
-              <span class="material-symbols-outlined">edit</span>
-            </div>
-            <div class="book-delete">
-              <span class="material-symbols-outlined">delete</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-
-    const statusIndicator = bookdiv.querySelector(".status-indicator");
-    const bookDelete = bookdiv.querySelector(".book-delete");
-    const bookEdit = bookdiv.querySelector(".book-edit");
-
-    statusIndicator.onclick = function () {
-      book.changeStatus();
-      bookscontainer.innerHTML = "";
-      books.forEach((book) => {
-        renderBook(book);
-      });
-    };
-
-    bookDelete.onclick = function () {
-      book.Remove()
-      bookscontainer.innerHTML = "";
-      books.forEach((book) => {
-        renderBook(book);
-      });
-    }
-
-    bookEdit.onclick = function () {
-
-   book.Edit();
-
-  
-dialog.showModal();
-
-      
  
 
 
 
-
-    };
-
-    bookscontainer.appendChild(bookdiv);
-
-  }
-  bookscontainer.innerHTML = "";
-
-
-
-
-
-  books.forEach((book) => {
-    renderBook(book);
-    
-  });
+  renderBooks()
 
   booksform.reset();
 };
@@ -217,3 +223,5 @@ dialogb.onclick = function () {
 dialogclose.onclick = function () {
   dialog.close();
 };
+
+
